e9a001d0c789e65ceb32fe7bc63c83f669dd7a68
Link <caulfieldd17@gmail.com>
2026-02-24 15:51:12 +0200

    feat: implement user DTOs and validation, add app configuration, and update main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	await app.listen(process.env.PORT ?? 3000);
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			forbidNonWhitelisted: true,
+			whitelist: true,
+			transform: true,
+		}),
+	);
+
+	const configService = app.get(ConfigService);
+
+	const PORT = configService.get<string>('PORT') || 3000;
+
+	await app.listen(PORT, () => {
+		// eslint-disable-next-line no-console
+		console.log(`Server is running on port ${PORT}`);
+	});
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Body, Patch, Param, Delete, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { GetUserIdParamDto } from './dto/get-user-id-param.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -9,7 +10,8 @@ export class UsersController {
 
 	@Post()
 	create(@Body() createUserDto: CreateUserDto) {
-		return this.usersService.create(createUserDto);
+		// return this.usersService.create(createUserDto);
+		return createUserDto;
 	}
 
 	@Get()
@@ -18,8 +20,8 @@ export class UsersController {
 	}
 
 	@Get(':id')
-	findOne(@Param('id') id: string) {
-		return this.usersService.findOne(+id);
+	getUsers(@Param() getUserParamDto: GetUserIdParamDto) {
+		return getUserParamDto;
 	}
 
 	@Patch(':id')
